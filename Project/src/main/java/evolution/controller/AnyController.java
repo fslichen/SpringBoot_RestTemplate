7977242b6cffb2948988b20e4c3ebd8c0acb02f8
fslic <fslic@DESKTOP-MCATCHJ>
2017-05-26 22:40:00 +0800

    new demos

--- Project/src/main/java/evolution/controller/AnyController.java
+++ Project/src/main/java/evolution/controller/AnyController.java
@@ -1,5 +1,8 @@
 package evolution.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +28,11 @@ public class AnyController {
 		return new ResponseDto(requestDto.getName());
 	}
 	
+	@PostMapping("/post/list")
+	public List<ResponseDto> postList(@RequestBody RequestDto requestDto) {
+		return Arrays.asList(new ResponseDto(requestDto.getName()));
+	}
+	
 	@GetMapping("/send/get")
 	public void sendGet() {
 		ResponseDto responseDto = restTemplate.getForObject("http://localhost:8080/get", 
@@ -36,5 +44,13 @@ public class AnyController {
 	public void sendPost() {
 		ResponseDto responseDto = restTemplate.postForObject("http://localhost:8080/post", new RequestDto("Ling"), ResponseDto.class);
 		System.out.println(responseDto);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@GetMapping("/send/post/list")
+	public void sendPostList() {
+		// Spring boot rest template is smart enough to do the transfer.
+		List<ResponseDto> responseDtos = restTemplate.postForObject("http://localhost:8080/post/list", new RequestDto("Ling"), List.class);
+		System.out.println(responseDtos);
 	}
 }
